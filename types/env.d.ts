56e81460676c8ef0adff84bed7ca7a1bc4253d96
Jeethan Roche <jeethanroche@gmail.com>
2025-12-27 14:17:30 +0530

    chore: add env types

---
+++ types/env.d.ts
@@ -0,0 +1,14 @@
+declare module "@env" {
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
+  export const SERVER_IP: string;
+  export const TWILIO_ACCOUNT_SID: string;
+  export const TWILIO_AUTH_TOKEN: string;
+  export const TWILIO_PHONE_NUMBER: string;
+  export const TWILIO_WHATSAPP_NUMBER: string;
+}
