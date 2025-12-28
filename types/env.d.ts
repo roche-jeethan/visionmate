4c23992feefac286e9d2f6731fba904615e9450f
Jeethan Roche <jeethanroche@gmail.com>
2025-12-28 18:05:29 +0530

    fix: update env variables for invalidation

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,11 +1,11 @@
 declare module "@env" {
-  export const FIREBASE_API_KEY: string;
-  export const FIREBASE_AUTH_DOMAIN: string;
-  export const FIREBASE_PROJECT_ID: string;
-  export const FIREBASE_STORAGE_BUCKET: string;
-  export const FIREBASE_MESSAGING_SENDER_ID: string;
-  export const FIREBASE_APP_ID: string;
-  export const FIREBASE_MEASUREMENT_ID: string;
+  export const EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+  export const EXPO_PUBLIC_FIREBASE_API_KEY: string;
+  export const EXPO_PUBLIC_FIREBASE_PROJECT_ID: string;
+  export const EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+  export const EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: string;
+  export const EXPO_PUBLIC_FIREBASE_APP_ID: string;
+  export const EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID: string;
   export const SERVER_IP: string;
   export const TWILIO_ACCOUNT_SID: string;
   export const TWILIO_AUTH_TOKEN: string;
