6199d1cfd604d059901060453b964022e35c257b
Jeethan Roche <jeethanroche@gmail.com>
2026-01-29 19:46:04 +0530

    merge: integrate code of aashna's face recognition

--- backend/app.py
+++ backend/app.py
@@ -15,14 +15,13 @@ from twilio_calls import router as twilio_router
 import time
 import logging
 
-
 class TranslationRequest(BaseModel):
     text: str
     target_lang: str
 
 
 load_dotenv()
-SERVER_IP = os.getenv("EXPO_PUBLIC_SERVER_IP")
+SERVER_IP = os.getenv("SERVER_IP")
 if not SERVER_IP:
     raise ValueError("SERVER_IP not found in environment variables")
 
